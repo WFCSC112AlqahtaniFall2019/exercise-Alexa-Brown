a15fdf507289ed7bacc1909e03dcd283cc27e0b9
Alexa-Brown <browam18@wfu.edu>
2019-09-05 16:15:13 -0400

    Changed arrays into vectors, swapped the vector using vectors

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];// User values
+    vector<int>revVctr(8);// User values
     int i;                        // Loop index
     int temp = 0;           //temp variable
 
     cout << "Enter " << NUM_ELEMENTS << " integer values separately, pressing enter after each one..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+        cin >> revVctr.at(i);
     }
     for (i=0; i < (NUM_ELEMENTS/2)  ; ++i){
-        temp = revVctr[i];
-        revVctr[i] = revVctr[(NUM_ELEMENTS - i )-1 ];
-        revVctr[NUM_ELEMENTS-i -1 ] = temp;
+        temp = revVctr.at(i);
+        revVctr.at(i) = revVctr.at((NUM_ELEMENTS - i )-1);
+        revVctr.at((NUM_ELEMENTS - i )-1) = temp;
     }
 
     // Print values
     cout << endl << "New values: ";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+        cout << " " << revVctr.at(i);
     }
     cout << endl;
 
